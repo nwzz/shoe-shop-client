8677f9c792eb55410be115eee18a194640d6ebf2
nwzz <shahnewaz@mail.com>
2022-02-06 18:06:45 +0600

    Updates

---
+++ static/js/62.37d0a943.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshoe_shop_client=self.webpackChunkshoe_shop_client||[]).push([[62],{8062:function(e,n,s){s.r(n);s(2791);var h=s(184);n.default=function(){return(0,h.jsx)("div",{children:(0,h.jsx)("h3",{children:"this is landing"})})}}}]);
+//# sourceMappingURL=62.37d0a943.chunk.js.map
